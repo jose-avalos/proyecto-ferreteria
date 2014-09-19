a0d0fda7aa0b9a899eab0b21d818fd7958eeee23
chack121 <chack121_k7@hotmail.com>
2014-09-18 21:02:49 -0600

    creacion

---
+++ suministros-ferreteros/web/js/login.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+	$(".log").click(function(){	
+		
+ 		$("#dialogo").dialog({title:"Iniciar Sesion",width: 250, height: 250, show: "scale",hide: "scale", resizable: "false", modal: "true",closeOnEscape: true});
+    });
+ 
+	});
